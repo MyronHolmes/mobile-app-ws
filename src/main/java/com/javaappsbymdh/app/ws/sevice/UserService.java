dd77c98699164722ee8153bbc7890e9ec28bd81c
Myron Holmes <1dequanh@gmail.com>
2023-10-11 14:51:58 -0400

    added UserService and UserserviceImpl

---
+++ src/main/java/com/javaappsbymdh/app/ws/sevice/UserService.java
@@ -0,0 +1,7 @@
+package com.javaappsbymdh.app.ws.sevice;
+
+import com.javaappsbymdh.app.ws.shared.dto.UserDto;
+
+public interface UserService {
+   UserDto createUser(UserDto user);
+}
---
+++ src/main/java/com/javaappsbymdh/app/ws/sevice/impl/UserServiceImpl.java
@@ -0,0 +1,11 @@
+package com.javaappsbymdh.app.ws.sevice.impl;
+
+import com.javaappsbymdh.app.ws.sevice.UserService;
+import com.javaappsbymdh.app.ws.shared.dto.UserDto;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public UserDto createUser(UserDto user) {
+        return null;
+    }
+}
